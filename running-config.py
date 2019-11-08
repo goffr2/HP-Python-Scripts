619725c0a62d2fbce8b3b96ac9b6d380bd347b92
Ricky Goff <55897197+goffr2@users.noreply.github.com>
2019-11-08 09:30:28 -0500

    Added Multi Threading plus Authentication prompt

--- running-config.py
+++ running-config.py
@@ -1,34 +1,57 @@
 from netmiko import ConnectHandler
+import threading
+from multiprocessing import Queue
+import getpass
 import sys
 
 with open('Network_Switch_IP_Address.list')as L:
 	IP_List = L.readlines()
+    
 L.close()
-UserName = sys.argv[1]
-UserName= str(UserName)
-Password = sys.argv[2]
-Password = str(Password)
-
+Username = input("Username: ")
+password = getpass.getpass()
 content= [x.strip('\n') for x in IP_List]
 
-for x in content:
+def ssh_session(x, output_q):
+    x = x.strip('\n')
+    try:
+        device_data = {
+            'device_type': 'hp_procurve',
+            'ip': x,
+            'username': Username,
+            'password': password,
+            }
 
-	device_data = {
-        	'device_type': 'hp_procurve',
-            	'ip': x,
-           	'username': UserName,
-           	'password': Password,
-        }
+        device = ConnectHandler(**device_data)
+        output = device.send_command("show running-config")
 
+        f = open(x+'_running_config.txt','w')
+        f.write (output)
+        f.close()
+        print (x +'s running config save is completed.')
+        device.disconnect()
+    except:
+        print('device '+x+ " is not reachable.")
 
+if __name__ == "__main__":
+    
+    output_q = Queue()
+    
+    for x in IP_List:
+          my_thread = threading.Thread(target=ssh_session, args=(x, output_q))
+          my_thread.start()
 
+    # Wait for all threads to complete
+    main_thread = threading.currentThread()
+    for some_thread in threading.enumerate():
+        if some_thread != main_thread:
+            some_thread.join()
 
-	device = ConnectHandler(**device_data)
-	output = device.send_command("show running-config")
-
-	f = open(x+'_running_config.txt','w')
-	f.write (output)
-	f.close()
-	print x +'s running config save is completed.'
-	device.disconnect()
-print 'completed'
+    # Retrieve everything off the queue - k is the router IP, v is output
+    # You could also write this to a file, or create a file for each router
+    
+    while not output_q.empty():
+        my_dict = output_q.get()
+        for k, val in my_dict.iteritems():
+            print (k)
+            print (val)
